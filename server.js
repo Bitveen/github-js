4ed33c5538cf25b27c3cf53d8ad6b84862093084
Max <bitveen@gmail.com>
2015-09-18 04:24:58 +0300

    Произвел рефакторинг сервера

--- server.js
+++ server.js
@@ -1,38 +1,24 @@
-/*
-* Сервер для обслуживания статических файлов
-* */
-var http = require('http');
-var fs   = require('fs');
-var path = require('path');
+var http = require('http'),
+    fs   = require('fs'),
+    path = require('path'),
+    EventEmitter = require('events').EventEmitter;
+
+var fileEmitter = new EventEmitter();
 
 var port = process.env.PORT || 8080;
-var pathToPublic = './public';
+var pathToPublic = 'public';
 
-var server = http.createServer(function(req, res) {
-    if (req.url === '/') {
-        // основной шаблон
-        serveHtml(res);
-    } else {
-        // стили, скрипты и картинки
-        serveStatic(req, res);
-    }
+/* Функция для обслуживания базового файла представления */
+function handleLayout(res) {
 
-});
-
-
-function serveStatic(req, res) {
-    if (req.url == '/favicon.ico') {
-        return;
-    }
-    var path = 'public' + req.url;
-    var extension = req.url.slice(req.url.lastIndexOf('.'));
-    fs.readFile(path, function(err, fileData) {
+    fs.readFile(pathToPublic + '/templates/layout.html', function(err, fileData) {
         if (err) {
+            fileEmitter.emit('fileError', 'Ошибка при чтении файла.');
             throw err;
         }
-        var type = getFileMimeType(extension);
         res.writeHead(200, {
-            'Content-Type': type
+            'Content-Type': 'text/html',
+            'Content-Length': Buffer.byteLength(fileData)
         });
         res.write(fileData);
         res.end();
@@ -40,36 +26,86 @@ function serveStatic(req, res) {
 
 }
 
-function serveHtml(res) {
-    fs.readFile('public/templates/layout.html', function(err, fileData) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
+/* Функция для обслуживания статических файлов: partials, css, js */
+function serveStatic(req, res) {
+    /* Чтобы Chrome не пытался грузить favicon */
+    if (req.url === '/favicon.ico') {
+        res.statusCode = 404;
+        res.end();
+        return;
+    }
+
+    var path = pathToPublic + req.url;
+
+    fs.readFile(path, function(err, fileData) {
+        if (err) {
+            throw err;
+        }
+        res.setHeader('Content-Length', Buffer.byteLength(fileData));
+        res.statusCode = 200;
+        writeStaticHeader(req, res);
         res.write(fileData);
         res.end();
     });
+
+}
+
+/* Функция для записи заголовка Content-Type на базе расширения файла */
+function writeStaticHeader(req, res) {
+
+    var extension = req.url.slice(req.url.lastIndexOf('.') + 1);
+    switch (extension) {
+        case 'html':
+            res.setHeader('Content-Type', 'text/html');
+            break;
+        case 'css':
+            res.setHeader('Content-Type', 'text/css');
+            break;
+        case 'js':
+            res.setHeader('Content-Type', 'text/javascript');
+            break;
+        default:
+            res.statusCode = 500;
+    }
+
 }
 
 
-function getFileMimeType(extension) {
-    var type;
-    switch (extension) {
-        case '.css':
-            type = 'text/css';
-            break;
-        case '.js':
-            type = 'text/javascript';
-            break;
+fileEmitter.on('mainLayoutExists', handleLayout);
+fileEmitter.on('staticFileExists', serveStatic);
+
+fileEmitter.on('fileError', function(reason) {
+    console.log(reason);
+});
+
+
+
+
+/* Сам сервер для обслуживания запросов */
+var server = http.createServer(function(req, res) {
+
+    if (req.method === 'GET' && req.url === '/') {
+
+        fs.exists(pathToPublic + '/templates/layout.html', function(exists) {
+            if (!exists) {
+                fileEmitter.emit('fileError', 'Файл отсутствует.');
+            }
+            fileEmitter.emit('mainLayoutExists', res);
+
+        });
+
+    } else {
+        fs.exists(pathToPublic + req.url, function(exists) {
+            if (!exists) {
+                fileEmitter.emit('fileError', 'Файл отсутствует.');
+            }
+            fileEmitter.emit('staticFileExists', req, res);
+
+        });
 
     }
-    return type;
-}
 
-
-function handle404(){}
-function handleHtml(){}
-function handleJs(){}
-function handleCss(){}
-
-
+});
 
 server.listen(port, function() {
     console.log('Server listening on ' + port);
